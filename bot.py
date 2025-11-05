cf87ea77856afec28839925fb379c44387da3ea7
David <kigeandaviddd@gmail.com>
2025-11-05 23:05:35 +0300

    commit

---
+++ bot.py
@@ -0,0 +1,27 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo, KeyboardButton,ReplyKeyboardMarkup, ReplyKeyboardRemove
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.client.default import DefaultBotProperties
+
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot('8441274720:AAG-RWg-JNaFC6hGC1aCjuFrdbqAn9tGm8Y', default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher()
+
+kb = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text='app', web_app=WebAppInfo(url=f''))],
+    ])
+
+
+@dp.message(CommandStart())
+async def func(message: Message):
+    await message.answer('Добро пожаловать',
+                            reply_markup=kb)
+
+
+async def start():
+    await dp.start_polling(bot)
